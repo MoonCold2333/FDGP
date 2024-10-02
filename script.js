bf76a4bade591039dab180bf067dd77ac933423f
MoonCold2333 <3374368360@qq.com>
2024-10-02 21:00:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function copyToClipboard(text) {
+  var textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  alert('电子邮件地址已复制到剪贴板');
+}
